cfaa6dff030d4993ce2ddcec41afc1d0f59abf87
Ashokgkumar <ashok91293@gmail.com>
2024-08-03 20:39:24 +0530

    Update Initial Code.java

--- Initial Code.java
+++ Initial Code.java
@@ -7,7 +7,7 @@ System.out.println("Audio function added");
 System.out.println("Document function added");
 System.out.println("All Options Added Successfully");
 System.out.println("Triggered");
-System.out.println("Githubb Testing");
+System.out.println("GithubTesting");
   
 }
 }
